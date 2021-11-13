01c37500df4a853af62e843d71dffad0ffd0babf
Cristian Daniel Morales Villada <cristian980829@gmail.com>
2021-11-12 23:53:45 -0500

    PROJECT CREATED

---
+++ src/StoriesApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const StoriesApp = () => {
+    return (
+        <div>
+            <h1>Stories App</h1>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { StoriesApp } from './StoriesApp';
+
+ReactDOM.render(
+    <StoriesApp />,
+  document.getElementById('root')
+);
+
