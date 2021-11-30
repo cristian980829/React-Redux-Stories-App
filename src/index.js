a9ca1117be3610bbb0437a7ad834d8f996266d85
Cristian Daniel Morales Villada <cristian980829@gmail.com>
2021-11-29 23:56:43 -0500

    REMOVE PREVIOUS SNACK

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { StoriesApp } from './StoriesApp';
-import { SnackbarProvider } from 'notistack';
 
 import './styles.css';
 
 ReactDOM.render(
-   <SnackbarProvider maxSnack={3}>
-      <StoriesApp />
-   </SnackbarProvider>,
+      <StoriesApp />,
   document.getElementById('root')
 );
 
